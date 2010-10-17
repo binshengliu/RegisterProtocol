10f8ebca9d9f26d39c626d1683552ba46bdb33f3
chris <bbshliu@gmail.com>
2010-10-17 13:19:25 +0800

    use a helper class

---
+++ App_Code/NameHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for NameHelper
+/// </summary>
+public class NameHelper
+{
+	public NameHelper()
+	{
+	}
+	public static string GetServerTypeColumnName(string serverName)
+	{
+		return serverName + "_type";
+	}
+
+	public static string GetRemoteClientTableName(string serverColumnName, string id)
+	{
+		CafeInformationHelper helper = CafeInformationHelper.GetCafeInformationHelper(id);
+		string remoteType = helper.Parameters[serverColumnName];
+		return remoteType;
+	}
+}
